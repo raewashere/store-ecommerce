596c0acc2ba638e5f40acadd147c8d1c6eff4f42
Raymundo Torres <rayplaneta21@gmail.com>
2025-02-22 23:56:26 -0600

    cambios de duracion y mensajes

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Importacion de componente CounterComponent
+import { CounterComponent } from "../../../shared/components/counter/counter.component";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CounterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  duration = signal(1000);
+  message = signal('Hello World');
 
+  changeDuration(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.duration.set(Number(value));
+  }
+
+  changeMessage(event: Event) {
+    const input = (event.target as HTMLInputElement).value;
+    this.message.set(input);
+  }
 }
