7e52fd6987288feffbcd82723066c097f55aa103
Raymundo Torres <rayplaneta21@gmail.com>
2025-02-24 21:52:38 -0600

    ejercicio de estados de componente en su ciclo de vida

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -18,11 +18,47 @@ export class CounterComponent {
     console.log('-'.repeat(10));
   }
 
-  ngChanges(changes: SimpleChanges) {
+  //Se ejecuta en cualquier cambio de las propiedades de entrada
+  ngOnChanges(changes: SimpleChanges) {
     //NO ASYNC
     //before rendering
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
     console.log(changes);
+
+    const duration  = changes['duration']; 
+    //Valida que el valor actual sea diferente al anterior
+    if (duration && duration.currentValue !== duration.previousValue) {
+      this.doSomething();
+    }
   }
+
+  //Puede demorar lo que sea, para consumir una API, por ejemplo
+  ngOnInit() {  
+    //ASYNC
+    //after rendering
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));
+    console.log('duration => ' + this.duration);
+    console.log('message => ' + this.message);
+  }
+
+  ngAfterViewInit() {
+    //ASYNC
+    //Despues de renderizar el contenido, se ejecuta despues de ngOnInit o que se carga todo el contenido inicial
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy() {
+    //ASYNC
+    //before destroying
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
+  }
+
+  doSomething() {
+    console.log('Cmbio de duracion');
+  }
+
 }
