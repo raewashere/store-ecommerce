30e55e8a6d8b10036d99bd08221faecc3d26eb29
Raymundo Torres <rayplaneta21@gmail.com>
2025-03-02 20:00:39 -0600

    implementacion de interfaz de productos y llamados de lista

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from './../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -9,6 +10,19 @@ import { ProductComponent } from '../../components/product/product.component';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts = [
+      { id: Date.now(), title: 'Product 1', price: 100, image: 'https://picsum.photos/640/640?r=23', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 2', price: 100, image: 'https://picsum.photos/640/640?r=21', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 3', price: 100, image: 'https://picsum.photos/640/640?r=22', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 4', price: 100, image: 'https://picsum.photos/640/640?r=24', creationAt: new Date().toISOString() },
+    ];
+    this.products.set(initProducts);
+  }
+
   fromChild(event: string){
     console.log('Event from child: ', event);
   }
