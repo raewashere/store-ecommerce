ca6c2a3e08483d48ae15ba37a8e0bab7676f0779
Raymundo Torres <rayplaneta21@gmail.com>
2025-03-03 19:58:28 -0600

    agrega header y modifica grid de productos

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from './../../../shared/models/product.model';
+import { HeaderComponent } from "../../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-list',
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -19,6 +20,27 @@ export class ListComponent {
       { id: Date.now(), title: 'Product 2', price: 100, image: 'https://picsum.photos/640/640?r=21', creationAt: new Date().toISOString() },
       { id: Date.now(), title: 'Product 3', price: 100, image: 'https://picsum.photos/640/640?r=22', creationAt: new Date().toISOString() },
       { id: Date.now(), title: 'Product 4', price: 100, image: 'https://picsum.photos/640/640?r=24', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 1', price: 100, image: 'https://picsum.photos/640/640?r=23', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 2', price: 100, image: 'https://picsum.photos/640/640?r=21', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 3', price: 100, image: 'https://picsum.photos/640/640?r=22', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 4', price: 100, image: 'https://picsum.photos/640/640?r=24', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 1', price: 100, image: 'https://picsum.photos/640/640?r=23', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 2', price: 100, image: 'https://picsum.photos/640/640?r=21', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 3', price: 100, image: 'https://picsum.photos/640/640?r=22', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 4', price: 100, image: 'https://picsum.photos/640/640?r=24', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 1', price: 100, image: 'https://picsum.photos/640/640?r=23', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 2', price: 100, image: 'https://picsum.photos/640/640?r=21', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 3', price: 100, image: 'https://picsum.photos/640/640?r=22', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 4', price: 100, image: 'https://picsum.photos/640/640?r=24', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 1', price: 100, image: 'https://picsum.photos/640/640?r=23', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 2', price: 100, image: 'https://picsum.photos/640/640?r=21', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 3', price: 100, image: 'https://picsum.photos/640/640?r=22', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 4', price: 100, image: 'https://picsum.photos/640/640?r=24', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 1', price: 100, image: 'https://picsum.photos/640/640?r=23', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 2', price: 100, image: 'https://picsum.photos/640/640?r=21', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 3', price: 100, image: 'https://picsum.photos/640/640?r=22', creationAt: new Date().toISOString() },
+      { id: Date.now(), title: 'Product 4', price: 100, image: 'https://picsum.photos/640/640?r=24', creationAt: new Date().toISOString() },
+    
     ];
     this.products.set(initProducts);
   }
