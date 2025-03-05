ff61458b3950b2b2e60a7cb8ae8889ebf7229d55
Raymundo Torres <rayplaneta21@gmail.com>
2025-03-04 23:59:01 -0600

    agrega items a barra desplegable y cuenta cantidad de items en carrito

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,14 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   //Estado del carrito visible o no
   hideSideMenu = signal(true);
+
+  //Recibe lista de productos del carrito
+  @Input({required:true}) cart: Product[] = [];
 
   //Funcion para mostrar u ocultar el carrito
   toggleSideMenu() {
