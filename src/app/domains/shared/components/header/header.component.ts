3dd5fbfe00fc176aef376bff3ce3cf811b241228
Raymundo Torres <rayplaneta21@gmail.com>
2025-03-05 23:48:08 -0600

    calculo de total

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
 
@@ -15,8 +15,24 @@ export class HeaderComponent {
   //Recibe lista de productos del carrito
   @Input({required:true}) cart: Product[] = [];
 
+  total = signal(0);
+
   //Funcion para mostrar u ocultar el carrito
   toggleSideMenu() {
     this.hideSideMenu.update(prevState => !prevState);
+  }
+
+  calcTotal() {
+    //Recorre el carrito y suma el precio de cada producto
+    return this.cart.reduce((total, product) => total + product.price, 0);
+  }
+
+  //Funcion que se ejecuta cuando se detectan cambios en
+  ngOnChanges0(changes : SimpleChanges) {
+    //Busca cambios en arreglo cart
+    const cart = changes['cart'];
+    if(cart) {
+      this.total.set(this.calcTotal());
+    }
   }
 }
