719abee636ea44ddf7c4a64550d4cb13805f6aca
Raymundo Torres <rayplaneta21@gmail.com>
2025-03-13 19:57:46 -0600

    uso de pipes en templates

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,9 +1,10 @@
 import { Component,EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '@shared/models/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
