1886c11cb38fa6f53c93c44725ac3315aacbcd0b
Raymundo Torres <rayplaneta21@gmail.com>
2025-03-18 23:56:16 -0600

    vistas anidadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,27 @@ import { Routes } from '@angular/router';
 import { ListComponent } from '@products/pages/list/list.component';
 import { AboutComponent } from '@info/pages/about/about.component';
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
         path: '',
-        component: ListComponent
-    },
-    {
-        path   : 'about',
-        component: AboutComponent
-    },
-    //Siempre al final para redirigir a la 404
-    {
-        path: '**',
-        component: NotFoundComponent
-    }
+        component: ListComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+    ],
+  },
+
+  //Siempre al final para redirigir a la 404
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
---
+++ src/app/domains/shared/components/layout/layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [CommonModule, HeaderComponent,RouterModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+}
