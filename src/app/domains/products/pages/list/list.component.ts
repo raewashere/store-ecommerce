17255c42310328d883bef0a87b0019aca0cc584b
Raymundo Torres <rayplaneta21@gmail.com>
2025-03-21 02:55:01 -0600

    inicio de filtro por categoria

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,34 +1,53 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, Input, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLinkWithHref } from '@angular/router';
 import { ProductComponent } from '@products/components/product/product.component';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
 
 @Component({
   selector: 'app-list',
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
 export class ListComponent {
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
 
   //Inyeccion de dependencias
   private cart = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
+  @Input() category_id?: string;
 
   ngOnInit() {
-    //Llamado al empezar renderizados
-    this.productService
-      .getProducts()
-      .subscribe({ 
-        next: (products) => this.products.set(products),
-        error: (error) => console.error(error),
-      });
+    this.getCategories();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+      this.getProducts();
   }
 
   addToCart(event: Product) {
     this.cart.addToCart(event);
+  }
+
+  private getProducts() {
+    //Llamado al empezar renderizados
+    this.productService.getProducts(this.category_id).subscribe({
+      next: (products) => this.products.set(products),
+      error: (error) => console.error(error),
+    });
+  }
+
+  private getCategories() {
+    this.categoryService.getAll().subscribe({
+      next: (data) => this.categories.set(data),
+      error: (error) => console.error(error),
+    });
   }
 }
--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -3,19 +3,25 @@ import { Injectable, inject } from '@angular/core';
 import { Product } from '../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
   private http = inject(HttpClient);
 
-  constructor() { }
+  constructor() {}
 
-  getProducts() {
-    return this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products');
+  getProducts(category_id?: string) {
+    const url = new URL('https://api.escuelajs.co/api/v1/products');
+    if (category_id) {
+      url.searchParams.set('categoryId', category_id);
+    }
+
+    return this.http.get<Product[]>(url.toString());
   }
 
   getOne(id: string) {
-    return this.http.get<Product>(`https://api.escuelajs.co/api/v1/products/${id}`);
+    return this.http.get<Product>(
+      `https://api.escuelajs.co/api/v1/products/${id}`
+    );
   }
 }
