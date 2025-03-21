12e916cc66d9f0a47d3ce65165801d46f2f7f53f
Raymundo Torres <rayplaneta21@gmail.com>
2025-03-21 00:02:00 -0600

    funcion de agregar a carrito desde detalle

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, Input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '@shared/services/product.service';
+import { CartService } from '@shared/services/cart.service';
 import { Product } from '@shared/models/product.model';
 
 @Component({
@@ -14,7 +15,7 @@ export class ProductDetailComponent {
   product = signal<Product | null>(null);
   cover = signal('');
   private productService = inject(ProductService);
-
+  private cartService = inject(CartService);
   constructor() {}
 
   ngOnInit() {
@@ -33,4 +34,13 @@ export class ProductDetailComponent {
   changeCover(image: string) {
     this.cover.set(image);
   } 
+
+
+  //Agregar al carrito
+  addToCart() {
+    const product = this.product();
+    if (product) {
+      this.cartService.addToCart(product);
+    }
+  }
 }
